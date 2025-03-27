sourcegraph/cody

Handle autoedit accept

jetbrains/src/main/kotlin/com/sourcegraph/cody/autocomplete/action/AcceptAutocompleteActionHandler.kt
@@ -17,7 +17,10 @@ class AcceptAutocompleteActionHandler : AutocompleteActionHandler() {
    */
   override fun doExecute(editor: Editor, maybeCaret: Caret?, dataContext: DataContext?) {
     val caret = maybeCaret ?: getSingleCaret(editor) ?: return
-    val completionItem = getCurrentAutocompleteItem(caret) ?: return
+    val completionItem =
+        getCurrentAutoeditItemAsCompletionItem(editor)
+            ?: getCurrentAutocompleteItem(caret)
+            ?: return
 
     AcceptCodyAutocompleteAction.tracker.set(completionItem.id)
     WriteAction.run<RuntimeException> { applyInsertText(editor, caret, completionItem) }

salewski/sourcegraph--cody

jetbrains/src/main/kotlin/com/sourcegraph/cody/autocomplete/action/AcceptAutocompleteActionHandler.kt
@@ -17,7 +17,10 @@ class AcceptAutocompleteActionHandler : AutocompleteActionHandler() {
    */
   override fun doExecute(editor: Editor, maybeCaret: Caret?, dataContext: DataContext?) {
     val caret = maybeCaret ?: getSingleCaret(editor) ?: return
-    val completionItem = getCurrentAutocompleteItem(editor) ?: return
+    val completionItem =
+        getCurrentAutoeditItemAsCompletionItem(editor)
+            ?: getCurrentAutocompleteItem(editor)
+            ?: return
 
     AcceptCodyAutocompleteAction.tracker.set(completionItem.id)
     WriteAction.run<RuntimeException> { applyInsertText(editor, caret, completionItem) }